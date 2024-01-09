41be0623ae007016343bae36b34bd850db599dd6
VictorAlejandreMoril <VictorAlejandreMoril@CSPM1PC14418-CA>
2024-01-09 12:11:57 +0100

    Mi segundo commit, añado una variable y mensaje.

--- TutorialJava/src/GitTutorial/PrimeraClase.java
+++ TutorialJava/src/GitTutorial/PrimeraClase.java
@@ -5,6 +5,12 @@ public class PrimeraClase {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Aquí va el código
+		
+		System.out.println("Hola mundo");
+		
+		String nombre = "Tomeu";
+		System.out.println("Hola + nombre");
+		
 	}
 
 }
